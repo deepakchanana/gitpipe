a0d2ab9d39d7ebc6931e5d9fe031d30397438b6e
deepakchanana <deepakdeep1992@gmail.com>
2020-04-29 14:13:13 -0700

    pushing firsttime

---
+++ pipeline/src/main/java/pipelineProject/pipeline/App.java
@@ -0,0 +1,13 @@
+package pipelineProject.pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pipeline/src/test/java/pipelineProject/pipeline/Class1.java
@@ -0,0 +1,12 @@
+package pipelineProject.pipeline;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public void tc()
+	{
+		System.out.println("I am testcase");
+	}
+}
